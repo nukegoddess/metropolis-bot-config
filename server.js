eda82f3945b90513eca4a63be21ea0e164b912a9
3formaggi <georg@georgdelp.de>
2020-07-25 13:04:32 +0200

    webserver updated

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+app.use(express.static("htdocs"));
+app.use(function (req, res, next) {
+    res.set('Cache-Control', 'no-cache');
+    next()
+})
 
 app.listen(process.env.PORT || 80, function () {
     console.log('App started!');
